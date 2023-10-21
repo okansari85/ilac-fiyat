5fe33bc033738729429a1895ca6776715cf4679a
okansari85 <sari_okan@hotmail.com>
2023-10-22 01:28:12 +0300

    wasd
    asda

--- app/.nuxt/types/nitro-routes.d.ts
+++ app/.nuxt/types/nitro-routes.d.ts
@@ -15,7 +15,7 @@ declare module 'nitropack' {
     '/api/_content/query': {
       'get': Simplify<Serialize<Awaited<ReturnType<typeof import('../../../node_modules/.pnpm/@nuxt+content@2.7.0/node_modules/@nuxt/content/dist/runtime/server/api/query').default>>>>
     }
-    '/api/_content/cache.1697927106717.json': {
+    '/api/_content/cache.1697927274365.json': {
       'get': Simplify<Serialize<Awaited<ReturnType<typeof import('../../../node_modules/.pnpm/@nuxt+content@2.7.0/node_modules/@nuxt/content/dist/runtime/server/api/cache').default>>>>
     }
     '/api/_content/navigation/:qid/**:params': {
