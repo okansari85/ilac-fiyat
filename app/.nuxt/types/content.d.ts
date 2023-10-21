6d1e57ea919574088cac4e1e2fe4b79f939cabfb
okansari85 <sari_okan@hotmail.com>
2023-10-22 01:24:36 +0300

    wasdas
    sadasd

---
+++ app/.nuxt/types/content.d.ts
@@ -0,0 +1,4 @@
+declare module '#content/server' {
+  const serverQueryContent: typeof import('C:/Users/zanba/Desktop/ilac/ilac-fiyat/node_modules/.pnpm/@nuxt+content@2.7.0/node_modules/@nuxt/content/dist/runtime/server').serverQueryContent
+  const parseContent: typeof import('C:/Users/zanba/Desktop/ilac/ilac-fiyat/node_modules/.pnpm/@nuxt+content@2.7.0/node_modules/@nuxt/content/dist/runtime/server').parseContent
+}
---
+++ app/.nuxt/types/layouts.d.ts
@@ -0,0 +1,7 @@
+import { ComputedRef, Ref } from 'vue'
+export type LayoutKey = "page"
+declare module "C:/Users/zanba/Desktop/ilac/ilac-fiyat/node_modules/.pnpm/nuxt@3.6.1_@types+node@18.0.0_eslint@8.43.0_sass@1.63.4_typescript@5.1.3_vue-tsc@1.8.1/node_modules/nuxt/dist/pages/runtime/composables" {
+  interface PageMeta {
+    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
+  }
+}
