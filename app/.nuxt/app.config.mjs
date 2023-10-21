a70ddf743073b9a4fd22c9ad1c1d5c5243beda75
okansari85 <sari_okan@hotmail.com>
2023-10-22 01:43:33 +0300

    asda
    sdasd

--- app/.nuxt/app.config.mjs
+++
@@ -1,9 +0,0 @@
-
-import { defuFn } from 'C:/Users/zanba/Desktop/ilac/ilac-fiyat/node_modules/.pnpm/defu@6.1.2/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-import cfg0 from "C:/Users/zanba/Desktop/ilac/ilac-fiyat/app/app.config.ts"
-import cfg1 from "C:/Users/zanba/Desktop/ilac/ilac-fiyat/app.config.ts"
-
-export default /* #__PURE__ */ defuFn(cfg0, cfg1, inlineConfig)
--- app/.nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = string
-declare module "C:/Users/zanba/Desktop/ilac/ilac-fiyat/node_modules/.pnpm/nuxt@3.6.1_@types+node@18.0.0_eslint@8.43.0_sass@1.63.4_typescript@5.1.3_vue-tsc@1.8.1/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
